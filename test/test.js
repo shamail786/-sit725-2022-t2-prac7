f89d29b944bfb0113bd1611447557331426a3be6
shamail786 <haider.shamail68@gmail.com>
2022-12-15 11:51:12 +1100

    testing

--- test/test.js
+++ test/test.js
@@ -40,4 +40,27 @@ describe("Add Two Numbers", function () {
       done();
     });
   });
+  describe("Add Two strings", function () {
+    var url = "http://localhost:3000/addTwoNumbers/firstNumber/secondNumber";
+    it("should return status 200", function (done) {
+      request(url, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+    it("returns statusCode key in body to check if api gives right result should be 400", function (done) {
+      request(url, function (error, response, body) {
+        body = JSON.parse(body);
+        expect(body.statusCode).to.equal(400);
+        done();
+      });
+    });
+    it("returns the result as null", function (done) {
+      request(url, function (error, response, body) {
+        body = JSON.parse(body);
+        expect(body.result).to.be.a("null");
+        done();
+      });
+    });
+  });
 });
