a6aae9746202fa2e59f46510bd177cf75c3c749a
shamail786 <haider.shamail68@gmail.com>
2022-12-15 11:29:57 +1100

    Testing

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use("/api/projects", projectRoutes);
 
+app.get("/addTwoNumbers/:firstNumber/:secondNumber", function (req, res, next) {
+  var firstNumber = parseInt(req.params.firstNumber);
+  var secondNumber = parseInt(req.params.secondNumber);
+  var result = firstNumber + secondNumber || null;
+  if (result == null) {
+    res.json({ result: result, statusCode: 400 }).status(400);
+  } else {
+    res.json({ result: result, statusCode: 200 }).status(200);
+  }
+});
+
 //mongodb connection....
 // const MongoClient = require("mongodb").MongoClient;
 // const uri =
