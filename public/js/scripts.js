8a4eab7bd9f20a37a6e7b51315e7f1912e4a4a10
shamail786 <haider.shamail68@gmail.com>
2023-01-17 12:01:44 +1100

    Applied Sockets

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -13,6 +13,12 @@
 //   },
 // ];
 
+// connect to the socket
+let socket = io();
+socket.on("number", (msg) => {
+  console.log("Random number: " + msg);
+});
+
 const getProjects = () => {
   $.get("/api/projects", (response) => {
     if (response.statusCode == 200) {
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var cors = require("cors");
 let projectCollection;
 let dbConnet = require("./dbConnect");
 let projectRoutes = require("./routes/projectRoutes");
-
+let http = require("http").createServer(app);
+let io = require("socket.io")(http);
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,6 +21,16 @@ app.get("/addTwoNumbers/:firstNumber/:secondNumber", function (req, res, next) {
   } else {
     res.json({ result: result, statusCode: 200 }).status(200);
   }
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  setInterval(() => {
+    socket.emit("number", parseInt(Math.random() * 10));
+  }, 1000);
 });
 
 //mongodb connection....
@@ -91,7 +102,7 @@ app.get("/addTwoNumbers/:firstNumber/:secondNumber", function (req, res, next) {
 // });
 
 var port = process.env.port || 3000;
-app.listen(port, () => {
+http.listen(port, () => {
   console.log("App listening to: " + port);
   //createCollection("Vehicles");
 });
